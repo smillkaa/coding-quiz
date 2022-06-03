ceb15ffa8cf43fb7160533e97c045fd2e729f3fd
Seleste <104727784+smillkaa@users.noreply.github.com>
2022-06-03 11:21:14 -0700

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+var startBtn = document.getElementById("start")
+var startContainer = document.getElementById("startContainer")
+
+startBtn.addEventListener("click", startQuiz)
+
+function startQuiz(){
+    startContainer.remove()
+
+}
+
+function nextQuestion (){
+
+}
+
+function selectAnswer(){
+
+}
